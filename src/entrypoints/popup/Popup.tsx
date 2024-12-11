31ef686115e6b2a7d3af36f199ce90fd49dc4c8e
Abhigyan Trips <contact@abhigyantrips.dev>
2024-12-11 18:14:33 +0530

    feat(ui): init popup

---
+++ src/entrypoints/popup/Popup.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { Bolt, Settings } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+
+import "tailwindcss-animate";
+
+import { Button } from "@/components/ui/button";
+
+import "@/styles/globals.css";
+
+import { isExtensionEnabled } from "@/lib/storage";
+import { cn } from "@/lib/utils";
+
+import extLogo from "/logo.svg";
+
+function Popup() {
+  const [enabled, setEnabled] = React.useState<boolean | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const isEnabled = await isExtensionEnabled.getValue();
+
+      setEnabled(isEnabled);
+    })();
+  }, []);
+
+  const toggleExtension = async () => {
+    const isEnabled = await isExtensionEnabled.getValue();
+
+    setEnabled(!isEnabled);
+    await isExtensionEnabled.setValue(!isEnabled);
+  };
+
+  return (
+    <>
+      <div className="flex min-h-[200px] w-[300px] flex-col items-center bg-background p-2">
+        <div className="absolute right-2 top-2">
+          <Button size="icon" variant="ghost" onClick={() => toggleExtension()}>
+            <Bolt />
+          </Button>
+        </div>
+        <div className="flex flex-col items-center justify-center space-y-2 px-2 py-4 text-foreground">
+          <img
+            src={extLogo}
+            className={cn(
+              "size-32 transition-all duration-300",
+              enabled ? "grayscale-0" : "grayscale"
+            )}
+            alt="Logo"
+          />
+          <span className="font-bold transition-all duration-300">
+            extension {enabled ? "enabled" : "disabled"}.
+            {!enabled && (
+              <>
+                {" "}
+                <span
+                  className="cursor-pointer underline underline-offset-2 hover:decoration-2"
+                  onClick={() => toggleExtension()}>
+                  enable it?
+                </span>
+              </>
+            )}
+          </span>
+          <p className="text-center text-xs">
+            you have used Instagram for <b>2,494 minutes today</b>. that is{" "}
+            <b>20%</b> of the day.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Popup />
+  </React.StrictMode>
+);
