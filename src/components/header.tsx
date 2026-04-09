ec218b1c1f467afee85a12adfe0cec8391d766ab
Abhigyan Trips <contact@abhigyantrips.dev>
2026-04-10 00:02:59 +0530

    chore(ui): remove unfinished button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,5 @@
 import "@fontsource/cookie/400.css";
 
-import { RiDiscordFill } from "@remixicon/react";
-
-import { Button } from "@/components/ui/button";
-
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "./ui/tooltip";
-
 export function Header() {
   return (
     <header className="bg-background/60 sticky inset-0 top-0 z-50 flex h-20 w-full flex-col items-center justify-around border-b backdrop-blur-[5px] backdrop-saturate-180">
@@ -22,22 +11,7 @@ export function Header() {
             Zengram
           </span>
         </div>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger>
-              <Button
-                variant="secondary"
-                className="group cursor-pointer hover:bg-purple-400/60"
-                asChild>
-                <a href="https://ko-fi.com/abhigyantrips" target="_blank">
-                  <RiDiscordFill className="size-5 transition-transform duration-350 group-hover:rotate-360" />
-                  Discord
-                </a>
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent>WAZZZUUUP</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+        <div />
       </nav>
     </header>
   );
